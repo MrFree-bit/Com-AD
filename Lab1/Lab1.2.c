440bde34303ccbedf6ca694ab3fa866fb7a20d3c
MrFree-bit <saksit0089@gmail.com>
2025-12-11 21:29:54 +0700

    Add lab 1.2

---
+++ Lab1/Lab1.2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GetSet(int **receiver, int *countPtr) {
+    int totalItems;
+    printf("Enter number of items: ");
+    scanf("%d", &totalItems);
+    int *tempBox = malloc(totalItems * sizeof(int));
+    for (int slot = 0; slot < totalItems; slot++) {
+        printf("Enter int only %d: ", slot + 1);
+        scanf("%d", &tempBox[slot]);
+    }    
+    *receiver = tempBox;
+    *countPtr = totalItems;
+}
+int main() {
+    int *data, num;
+    GetSet(&data, &num);
+
+    for (int i = 0; i < num; i++) {
+        printf("%d ", data[i]);
+    }   
+    free(data);
+    return 0;
+}
